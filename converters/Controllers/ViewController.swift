d067228c6cc10acb589a19bb8b9a1cd845704f72
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-23 02:16:46 -0300

    Convertendo as temperaturas digitadas pelo usuário

--- converters/Controllers/ViewController.swift
+++ converters/Controllers/ViewController.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet var lbAskTemperature: UILabel!
+    @IBOutlet var inputTemperature: UITextField!
+    @IBOutlet var lbResult: UILabel!
+    @IBOutlet var btConvertTemperature: UIButton!
+    @IBOutlet var pickerTemperature: UIPickerView!
+    
+    let dataArray = [String(localized: "celsius_to_fahrenheit"), String(localized: "fahrenheit_to_celsius")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initViews()
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let row = dataArray[row]
+        return row
+    }
+    
+    func initViews() {
+        btConvertTemperature.titleLabel?.text = "OK"
+        
+        inputTemperature.placeholder = String(localized: "type_temperature")
+        lbAskTemperature.text = String(localized: "convert_temperature")
+        lbResult.text = ""
+        
+        pickerTemperature.delegate = self as UIPickerViewDelegate
+        pickerTemperature.dataSource = self as UIPickerViewDataSource
+    }
+    
+    @IBAction func convertTemperature() {
+        let temperatureTyped = Double(inputTemperature.text ?? "0")
+        var temperatureConverted: Double = 0;
+        
+        let a = pickerTemperature.selectedRow(inComponent: 0)
+        switch a {
+            case 0:
+                temperatureConverted = TemperatureService.celisusToFahrenheit(temperature: temperatureTyped ?? 0)
+                break
+            
+            case 1:
+                temperatureConverted = TemperatureService.fahrenheitToCelsius(temperature: temperatureTyped ?? 0)
+                break
+            
+            default:
+                break
+        }
+        
+        lbResult.text = String(temperatureConverted)
+    }
+    
 }
 
