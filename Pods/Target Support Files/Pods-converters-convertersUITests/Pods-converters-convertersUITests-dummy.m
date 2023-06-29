51a17a3d1380e977fa9c982392c7870459786e36
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-06-29 03:03:26 -0300

    Adicionando a dependência Sheeeeeeeeet no projeto

---
+++ Pods/Target Support Files/Pods-converters-convertersUITests/Pods-converters-convertersUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_converters_convertersUITests : NSObject
+@end
+@implementation PodsDummy_Pods_converters_convertersUITests
+@end
---
+++ Pods/Target Support Files/Pods-converters/Pods-converters-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_converters : NSObject
+@end
+@implementation PodsDummy_Pods_converters
+@end
