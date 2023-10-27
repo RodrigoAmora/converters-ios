95ad37f895d1cffa432f971cb3d5b3a41959340c
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-10-27 03:08:55 -0300

    Criando a tela AboutViewController

---
+++ converters/Controllers/AboutViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AboutViewController.swift
+//  converters
+//
+//  Created by Rodrigo Amora on 27/10/23.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
