61f4cc9bd4061afc31d4dd469bd1af5071f05a9a
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-10-27 03:20:26 -0300

    Implementando a tela de sobre

--- converters/Controllers/AboutViewController.swift
+++ converters/Controllers/AboutViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class AboutViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var lbCreatedBy: UILabel!
+    @IBOutlet weak var lbName: UILabel!
+    @IBOutlet weak var lbEmail: UILabel!
+    
+    // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.initViews()
     }
 
+    // MARK: - Methods
+    private func initViews() {
+        self.lbCreatedBy.text = String(localized: "created_by")
+        self.lbName.text = String(localized: "created_by_name")
+        self.lbEmail.text = String(localized: "created_by_email")
+    }
 }
