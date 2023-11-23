f8a216b96700f98c217c709ffbe32ae944c959b0
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2023-11-23 02:07:18 -0300

    Criando marcadores

--- convertersTests/TemperatureTests.swift
+++ convertersTests/TemperatureTests.swift
@@ -9,7 +9,7 @@ import XCTest
 @testable import converters
 
 final class TemperatureTests: XCTestCase {
-
+    // MARK: - XCTestCase Methods
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,6 +18,7 @@ final class TemperatureTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    // MARK: - Test Methods
     func testsConverterCelsiustoFahrenheit() {
         let temperatureConverted = TemperatureService.celisusToFahrenheit(temperature: 0)
         let temperatureConverted2 = TemperatureService.celisusToFahrenheit(temperature: 100)
